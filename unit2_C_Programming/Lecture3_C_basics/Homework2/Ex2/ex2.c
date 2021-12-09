9a00099e9319c823e3e89b5c44390b36596c5ed6
mohamedabdallahtawfik14 <mohamedabdallahtawfik14@gmail.com>
2021-12-09 13:42:20 +0200

    including capital char

--- unit2_C_Programming/Lecture3_C_basics/Homework2/Ex2/ex2.c
+++ unit2_C_Programming/Lecture3_C_basics/Homework2/Ex2/ex2.c
@@ -15,10 +15,15 @@ int main(int argc, char **argv) {
 	switch(character)
 	{
 		case 'a' :
+		case 'A' :
 		case 'e' :
+		case 'E' :
 		case 'i' :
+		case 'I' :
 		case 'o' :
+		case 'O' :
 		case 'u' :
+		case 'U' :
 		{
 			printf("%c is a vowel", character);
 		}
