0e0aeae428f67c418bcd0d985810f10632b15034
Mohamed Abdallah Tawfik <mohamedabdallahtawfik14@gmail.com>
2021-12-25 17:58:30 +0200

    second condition is enough

--- unit2_C_Programming/codeforces/Way Too Long Words/main.c
+++ unit2_C_Programming/codeforces/Way Too Long Words/main.c
@@ -1,41 +1,41 @@
-/*
- * main.c
- *
- *  Created on: Dec 25, 2021
- *      Author: Mohamed
- */
+	/*
+	 * main.c
+	 *
+	 *  Created on: Dec 25, 2021
+	 *      Author: Mohamed
+	 */
 
-#include <stdio.h>
-#include <string.h>
+	#include <stdio.h>
+	#include <string.h>
 
-int main(int argc, char **argv)
-{
-	int noOfWords, count, internalCount, sum;
-	char word[100], out[3];
-	scanf("%d ", &noOfWords);
-	for (count = 0; count < noOfWords; ++count)
+	int main(int argc, char **argv)
 	{
-		sum = 0;
-		gets(word);
-		if(strlen(word) <= 10)
+		int noOfWords, count, internalCount, sum;
+		char word[100], out[3];
+		scanf("%d ", &noOfWords);
+		for (count = 0; count < noOfWords; ++count)
 		{
-			printf("%s\n", word);
-		}
-		else
-		{
-			out[0] = word[0];
-			out[2] = word[strlen(word) - 1];
-			internalCount = 1;
-			while((word[internalCount] != '\0') && (internalCount != (strlen(word) - 1)))
+			sum = 0;
+			gets(word);
+			if(strlen(word) <= 10)
 			{
-				sum++;
-				internalCount++;
+				printf("%s\n", word);
 			}
-			out[1] = sum;
-			printf("%c%d%c\n", out[0], out[1], out[2]);
-		}
+			else
+			{
+				out[0] = word[0];
+				out[2] = word[strlen(word) - 1];
+				internalCount = 1;
+				while(internalCount != (strlen(word) - 1))
+				{
+					sum++;
+					internalCount++;
+				}
+				out[1] = sum;
+				printf("%c%d%c\n", out[0], out[1], out[2]);
+			}
 
+		}
+		return 0;
 	}
-	return 0;
-}
 
