cacb886446be7a8dbc44ab8bda226c696ac5a193
Mohamed Abdallah Tawfik <mohamedabdallahtawfik14@gmail.com>
2022-02-11 16:24:03 +0200

    extended for lab 1

---
+++ Unit3_EmbeddedC/Unit3_EmbeddedC_lecture 3/Extended for lab1/app.c
@@ -0,0 +1,11 @@
+#include "uart.h"
+#include "platform_types.h"
+/* global array of char act as string holding our string which we need it to be written on uart */
+uint8 stringBuffer[100] = "Learn-in-Depth: Mohamed Abdallah";
+uint8 stringBuffer2[100] = "Learn-in-Depth: Mohamed Abdallah";
+void main(void)
+{
+    /* pass string to uart */
+    // name of array point to the address of first charater stringBuffer == &stringBuffer[0]
+    uart_send_string(stringBuffer); 
+}
---
+++ Unit3_EmbeddedC/Unit3_EmbeddedC_lecture 3/Extended for lab1/uart.h
@@ -0,0 +1,7 @@
+#ifndef _UART_H_
+#define _UART_H_
+#include "platform_Types.h"
+/* prototype */
+void uart_send_string(uint8 *p_tx_string);
+
+#endif
