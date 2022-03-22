75d846219e8eb3b96055ebe3f823cf767a8a6e6f
Mohamed Abdallah Tawfik <mohamedabdallahtawfik14@gmail.com>
2022-03-22 16:54:27 +0200

    toggle led Assignment

---
+++ Unit7/lecture1_lab2/main.c
@@ -0,0 +1,43 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 22, 2022
+ *      Author: Mohamed
+ */
+
+#include "platform_Types.h"
+
+//GPIO
+#define GPIO_BASE 0x20
+//PORTD
+#define PORTD 	*(vuint32_t*) (GPIO_BASE + 0x12)
+#define DDRD 	*(vuint32_t*) (GPIO_BASE + 0x11)
+
+void gpio_init()
+{
+	//Pins 4,5,6,7 output in portD
+	DDRD |= 0xF0;
+}
+
+void delay(vuint32_t x)
+{
+	vuint32_t i, j;
+	for (i = 0; i < x; ++i)
+		for (j = 0; j < 255; ++j);
+}
+int main(void)
+{
+	gpio_init();
+	while(1)
+	{
+		PORTD ^= (1<<5);
+		delay(500);
+		PORTD ^= (1<<6);
+		delay(500);
+		PORTD ^= (1<<7);
+		delay(500);
+		PORTD |= (1<<4);
+		delay(500);
+		PORTD  &= ~(1<<4);
+	}
+}
